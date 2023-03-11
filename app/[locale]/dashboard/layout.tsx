ee7bc7167209990cbb40ca623e340aef2d9ed873
CorentinGS <c.giaufersaubert@outlook.com>
2023-03-11 14:13:22 +0100

    next !!

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -7,6 +7,10 @@ import React from "react";
 import {cookies} from "next/headers";
 import {isConnected} from "@/common/utils/security";
 
+export const metadata = {
+    title: "Dashboard",
+};
+
 export default async function DashboardLayout({
                                                   // @ts-ignore
                                                   children,
