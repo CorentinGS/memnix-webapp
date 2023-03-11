a1e0fe25de86bd4e478e0b5e844e8646d260bb5e
CorentinGS <c.giaufersaubert@outlook.com>
2023-03-11 14:13:41 +0100

    next !!

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -1,46 +1,46 @@
 import "@/app/global.css";
 
-import {NextIntlClientProvider, useLocale} from "next-intl";
-import {notFound, redirect} from "next/navigation";
+import { NextIntlClientProvider, useLocale } from "next-intl";
+import { notFound, redirect } from "next/navigation";
 import Header from "@components/Global/Header";
 import React from "react";
-import {cookies} from "next/headers";
-import {isConnected} from "@/common/utils/security";
+import { cookies } from "next/headers";
+import { isConnected } from "@/common/utils/security";
 
 export const metadata = {
-    title: "Dashboard",
+  title: "Dashboard",
 };
 
 export default async function DashboardLayout({
-                                                  // @ts-ignore
-                                                  children,
-                                                  // @ts-ignore
-                                                  params,
-                                              }) {
-    const locale = useLocale();
+  // @ts-ignore
+  children,
+  // @ts-ignore
+  params,
+}) {
+  const locale = useLocale();
 
-    // Show a 404 error if the user requests an unknown locale
-    if (params.locale !== locale) {
-        notFound();
-    }
+  // Show a 404 error if the user requests an unknown locale
+  if (params.locale !== locale) {
+    notFound();
+  }
 
-    const messages = (await import(`../../../messages/${locale}.json`)).default;
+  const messages = (await import(`../../../messages/${locale}.json`)).default;
 
-    const jwt = cookies().get("jwt")?.value;
-    if (!jwt || jwt && (!await isConnected(jwt))) {
-        redirect("/auth");
-    }
+  const jwt = cookies().get("jwt")?.value;
+  if (!jwt || (jwt && !(await isConnected(jwt)))) {
+    redirect("/auth");
+  }
 
-    return (
-        <>
-            <Header/>
-            <section className="py-6 sm:py-8 lg:py-12">
-                <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-                    <NextIntlClientProvider locale={locale} messages={messages}>
-                        {children}
-                    </NextIntlClientProvider>
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <section className="py-6 sm:py-8 lg:py-12">
+        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <NextIntlClientProvider locale={locale} messages={messages}>
+            {children}
+          </NextIntlClientProvider>
+        </div>
+      </section>
+    </>
+  );
 }
--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 
 export default async function DashboardPage() {
-
-    return (
-        <>
-            <div className="flex flex-col items-center justify-center w-full h-full">
-                <h1 className="text-4xl font-bold">Dashboard</h1>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <h1 className="text-4xl font-bold">Dashboard</h1>
+      </div>
+    </>
+  );
 }
